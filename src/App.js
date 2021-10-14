b3c453914ef0d643d691f2af25243015209776a0
unknown <brnoamrr@gmail.com>
2021-10-14 01:18:45 -0300

    envio atv02

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Message } from "./mensagem.js";
+
+function App() {
+  return (
+    <div className="App">
+ 
+   <Message></Message>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mensagem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Message = () => (
+  <div>
+    <h1>O amanhã será melhor!</h1>
+  </div>
+);
